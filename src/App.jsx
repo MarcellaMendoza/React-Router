44e528a1d8a06648cc648c9cd7c088ba9fe8fb55
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-19 06:19:15 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Components/Home.jsx'
+import Contact from './Components/Contact.jsx'
+import About from './Components/About.jsx'
+import NotFound from './Components/NotFound.jsx'
+
+function App() {
+  return (
+    
+  {/* Delete this comment and add your code here! */}
+
+  
+
+  );
+}
+
+export default App;
+
+
+
+// Instructions
+
+// In this lesson, you will modify the code in 'App.jsx' to show different elements at different routes using React Router. The Home component should show at the / route, the About component should show at the /about route, and the Contact component should show at the /contact route. Finally, the NotFound component should show at all other routes.
+
+// Once you are done, try creating a new component and showing it at a route of your choice.
